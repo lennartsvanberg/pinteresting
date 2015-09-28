3f8f81bbad71d3efcc5e911a34fa0d554696a7c0
Lennart Svanberg <lennartsvanberg@gmail.com>
2015-09-28 20:34:04 +0200

    Add image upload with Paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
   # Note to set this to your actual host
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Paperclip
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin/convert"
+
 end
