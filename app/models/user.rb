1326b6271c9222ae6eddc0a92eeca440509817cb
Lennart Svanberg <lennartsvanberg@gmail.com>
2015-10-06 11:04:23 +0200

    Add names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-	has_many	:pins
+	
 
   	# Include default devise modules. Others available are:
   	# :confirmable, :lockable, :timeoutable and :omniauthable
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    has_many	:pins
+
+    validates :name, presence: true
+    
 end
